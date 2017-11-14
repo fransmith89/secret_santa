83e9ae0b39bfecfd5988e9efeb1b1e606b96bd12
Fran Smith <3216721+fransmith89@users.noreply.github.com>
2017-11-14 13:33:00 +0000

    [add] input validation

--- secret_santa/input.py
+++ secret_santa/input.py
@@ -1,8 +1,38 @@
+from secret_santa.error import InputError
+
+
 def get_names_list_from_input():
     user_message = (
         "To get your Secret Santa list, enter the names of everyone participating."
         "\n(Note: You will need at least 2 names and each name must be separated by a comma):\n"
     )
-    names = input(user_message)
-    names_list = [name.strip() for name in names.split(',')]
-    return names_list
+    while user_message is not None:
+        names = input(user_message)
+        # strip any extra space because ' bill' looks odd
+        names_list = [name.strip() for name in names.split(',')]
+        try:
+            validate_input(names_list)
+            return names_list
+        except InputError as err:
+            user_message = err.message
+
+
+def validate_input(names_list):
+    if len(names_list) < 2:
+        raise InputError(
+            "You need to enter at least 2 names, with each name separated by a comma,"
+            " for a secret santa list to be generated\n:"
+        )
+    elif '' in names_list or ' ' in names_list:
+        raise InputError(
+            "You entered empty spaces. You need to enter at least 2 names, with each"
+            " name separated by a comma, for a secret santa list to be generated\n:"
+        )
+    elif len(names_list) > len(set(names_list)):
+        # This ignores case so Sarah and sarah won't error but they are distinguishable
+        raise InputError(
+            "There are some duplicate names, add in a surname or some other way to identify them."
+            "Remember You need to enter at least 2 names, with each name separated by a comma,"
+            " for a secret santa list to be generated\n:"
+        )
+    return None
--- tests/test_input.py
+++ tests/test_input.py
@@ -1,6 +1,9 @@
 from unittest.mock import patch
 
-from secret_santa.input import get_names_list_from_input
+import pytest
+
+from secret_santa.error import InputError
+from secret_santa.input import get_names_list_from_input, validate_input
 
 
 @patch('builtins.input')
@@ -22,18 +25,46 @@ def test_get_names_list_from_input_returns_2_items_when_2_names_given_with_comma
 
 
 @patch('builtins.input')
-def test_get_names_list_from_input_returns_1_items_when_2_names_given_with_no_comma(input_mock):
-    input_mock.return_value = 'bill ben'
-
-    res = get_names_list_from_input()
-
-    assert(len(res) == 1)
-
-
-@patch('builtins.input')
-def test_get_names_list_from_input_returns_strips_white_space_from_name(input_mock):
-    input_mock.return_value = 'bill, ben'
+def test_get_names_list_from_input_strips_white_space_from_name(input_mock):
+    input_mock.return_value = 'bill, ben '
 
     res = get_names_list_from_input()
 
     assert(res[1] == 'ben')
+
+
+@patch('builtins.input')
+def test_get_names_list_from_input_only_returns_valid_data(input_mock):
+    # A couple of invalid entries followed by a valid one
+    input_mock.side_effect = ['bill ben', 'bill, ', 'bill, ben']
+
+    res = get_names_list_from_input()
+
+    assert(res == ['bill' , 'ben'])
+
+
+def test_validate_input_returns_none_when_valid_list_given():
+    names_list = ['bill', 'ben']
+
+    res = validate_input(names_list)
+
+    assert(res == None)
+
+
+@pytest.mark.parametrize('names_list', [['bill'], []])
+def test_validate_input_raises_error_when_less_than_2_names_given(names_list):
+    with pytest.raises(InputError):
+        validate_input(names_list)
+
+
+@pytest.mark.parametrize('names_list', [['bill', '', 'ben'], [' ', 'bill']])
+def test_validate_input_raises_error_when_empty_names_in_list(names_list):
+    with pytest.raises(InputError):
+        validate_input(names_list)
+
+
+def test_validate_input_raises_error_when_duplicate_names_in_list():
+    names_list = ['bill', 'ben', 'bill']
+
+    with pytest.raises(InputError):
+        validate_input(names_list)
